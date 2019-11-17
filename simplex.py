547f19c51476e78d65a961f2328e7287dfc1cccb
Nerdkz <viniciusmendonca@gec.inatel.br>
2019-11-17 04:42:56 -0300

    simplex.py is already solvin ppl problemsgit add .

--- simplex.py
+++ simplex.py
@@ -2,47 +2,83 @@ import numpy as np
 
 
 # function to find pivor column
-def pivoColum(matrix, lines, columns):
+def findPivoColum(matrix, columns):
+
+    menorCP = 0
+
+    for column in range(0, columns):
+        if matrix[0][column] < menorCP:
+            indiceCP = column
+            colunaPivo = matrix[:, column]
+            colunaPivo = np.reshape(colunaPivo, (3, 1))
+
+    return colunaPivo, indiceCP
+
+
+#function to find pivor line and pivor element
+def findPivoLine(matrix, indexCP, lines, columns):
+
+    minValue = float(99999.0)
+
+    for line in range(0, lines):
+
+        if matrix[line][indexCP] > 0 and matrix[line][columns - 1] > 0 :
+
+            if (matrix[line][columns - 1] / matrix[line][indexCP]) < minValue:
+                indiceLP = line
+                linhaPivo = matrix[line, :]
+                minValue = (matrix[line][columns - 1] / matrix[line][indexCP])
+
+    return linhaPivo, indiceLP
+
+
+
+def generateMatrix(matrix, lines, columns, columnPivor, linePivor, indexCP, indexLP):
+
+    elementPivor = matrix[indexLP][indexCP]
+    lineRef = linePivor / elementPivor
 
     for line in range(0, lines):
         for column in range(0, columns):
-            if matrix[line][column] < menorCP:
-                colunaPivo = matrix[:, column]
-                colunaPivo = np.reshape(colunaPivo, (3, 1))
-    return colunaPivo
 
-def pivoLine(matrix, lines, columns, colunaPivo):
+            if line != indexLP:
+                newline = matrix[line, :] + (((- 1) * columnPivor[line][0]) * lineRef)
+                matrix[line][:] = np.around(newline, 2)
 
-    ladoDireito = rightSide(matrix)
-    colunaPivo = colunaPivo
+            else:
+                matrix[line][:] = lineRef
 
-    
+    #print("{}\n\n\n".format(matrix))
 
 
 
-def rightSide(matrix):
-
-    ladoDireito = matrix[:, 6]
-    ladoDireito = np.reshape(ladoDireito, (3, 1))
-    return ladoDireito
+def multMatrix(a, b):
+    result = [[sum(x*y for x,y in zip(a_row,b_col)) for b_col in zip(*b)] for a_row in a]
+    return result
 
 
+# Main Function
 if __name__ == '__main__':
 
-    menorCP = 0
-    colunaPivo = []
-    linhaPivo = []
-    elementoPivo = 0
+    np.set_printoptions(precision= 3, suppress = True)
 
-    matrix = np.array([[-7, -7, -6, -9, 0, 0, 0],
-                       [4, 5, 3, 5, 1, 0, 30000],
-                       [2, 1.5, 3, 3, 0, 1, 20000]]);
+    colunaPivo = None
+    linhaPivo = None
+
+    matriz = np.array([[-5, -7, -8, 0, 0, 0],
+                       [1, 1, 2, 1, 0, 1190],
+                       [3, 4.5, 1, 0, 1, 4000]]);
 
 
-    lines = matrix.shape[0]
-    columns = matrix.shape[1]
+    matriz.astype(float)
 
-    matrix.astype(float)
+    linhasMatriz = matriz.shape[0]
+    colunasMatriz = matriz.shape[1]
 
-    colunaPivo = pivoColum(matrix, lines, columns)
-    linhaPivo = pivoLine(matrix, lines, columns, colunaPivo)
+    while min(matriz[0, :]) < 0:
+        colunaPivo, indiceCP = findPivoColum(matriz, colunasMatriz)
+        linhaPivo, indiceLP = findPivoLine(matriz, indiceCP, linhasMatriz, colunasMatriz)
+        generateMatrix(matriz, linhasMatriz, colunasMatriz, colunaPivo, linhaPivo, indiceCP, indiceLP)
+
+
+    print("{}\n\n\n".format(matriz))
--- simplexMethod.py
+++ simplexMethod.py
@@ -1,20 +1,21 @@
 import pulp
 problem = pulp.LpProblem("Farmer", pulp.LpMaximize)
 
-a = pulp.LpVariable('a', lowBound=0, cat='Continuous')
-b = pulp.LpVariable('b', lowBound=0, cat='Continuous')
-c = pulp.LpVariable('c', lowBound=0, cat='Continuous')
+m1 = pulp.LpVariable('m1', lowBound=0, cat='Continuous')
+m2 = pulp.LpVariable('m2', lowBound=0, cat='Continuous')
+m3 = pulp.LpVariable('m3', lowBound=0, cat='Continuous')
+m4 = pulp.LpVariable('m4', lowBound=0, cat='Continuous')
 
-
-problem += 5 * a + 7 * b + 8 * c, "Z"
-problem += 1 * a + 1 * b + 2 * c <= 1190
-problem += 3 * a + 4.5 * b + 1 * c <= 4000
+problem += 7 * m1 + 7 * m2 + 6 * m3 + 9 * m4, "Z"
+problem += 4 * m1 + 5 * m2 + 3 * m3 + 5 * m4 <= 30000
+problem += 2 * m1 + 1.5 * m2 + 3 * m3 + 3 * m4 <= 20000
 
 problem.solve()
 
 print("Problem status: " + pulp.LpStatus[problem.status])
-print("Optimal A: "+str(a.value()))
-print("Optimal B: "+str(b.value()))
-print("Optimal C: "+str(c.value()))
+print("Optimal M1: "+str(m1.value()))
+print("Optimal M2: "+str(m2.value()))
+print("Optimal M3: "+str(m3.value()))
+print("Optimal M4: "+str(m4.value()))
 
 print("By growing them the profit will be: "+str(pulp.value(problem.objective)))
